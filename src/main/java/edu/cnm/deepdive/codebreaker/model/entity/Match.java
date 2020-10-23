7b8605280b93b4148a3b0e25ef2c18cc244df09d
Nick Bennett <nick@nickbenn.com>
2020-10-23 10:03:21 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Match.java
@@ -0,0 +1,36 @@
+package edu.cnm.deepdive.codebreaker.model.entity;
+
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.lang.NonNull;
+
+@Entity
+public class Match {
+
+  @NonNull
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(name = "match_id", nullable = false, updatable = false,
+      columnDefinition = "CHAR(16) FOR BIT DATA")
+  private UUID id;
+
+  @NonNull
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  private Date started;
+
+  public enum Criterion {
+    GUESSES_TIME, TIME_GUESSES
+  }
+
+}
