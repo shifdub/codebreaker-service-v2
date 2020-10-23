4c1fd89a07218666270f008bbe303b025a60a12d
Nick Bennett <nick@nickbenn.com>
2020-10-23 12:13:56 -0600

    Implemented many-to-many.

--- src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Match.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Match.java
@@ -1,18 +1,37 @@
 package edu.cnm.deepdive.codebreaker.model.entity;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.lang.NonNull;
 
+@SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
+@Table(
+    indexes = {
+        @Index(columnList = "codeLength"),
+        @Index(columnList = "gameCount"),
+        @Index(columnList = "started,deadline")
+    }
+)
 public class Match {
 
   @NonNull
@@ -28,6 +47,116 @@ public class Match {
   @Temporal(TemporalType.TIMESTAMP)
   @Column(nullable = false, updatable = false)
   private Date started;
+
+  @Column(updatable = false)
+  private int codeLength;
+
+  @NonNull
+  @Column(nullable = false, updatable = false)
+  private String pool;
+
+  @Column(updatable = false)
+  private int gameCount;
+
+  @NonNull
+  @Column(nullable = false, updatable = false)
+  private Criterion criterion;
+
+  @NonNull
+  @Column(nullable = false, updatable = false)
+  private Date deadline;
+
+  @NonNull
+  @ManyToOne(fetch = FetchType.EAGER, optional = false)
+  @JoinColumn(name = "originator_id", nullable = false, updatable = false)
+  private User originator;
+
+  @ManyToOne(fetch = FetchType.EAGER)
+  @JoinColumn(name = "winner_id")
+  private User winner;
+
+  @NonNull
+  @ManyToMany(fetch = FetchType.EAGER,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinTable(joinColumns = {@JoinColumn(name = "match_id")},
+      inverseJoinColumns = {@JoinColumn(name = "user_id")})
+  @OrderBy("displayName ASC")
+  private final List<User> players = new LinkedList<>();
+
+  @NonNull
+  public UUID getId() {
+    return id;
+  }
+
+  @NonNull
+  public Date getStarted() {
+    return started;
+  }
+
+  public int getCodeLength() {
+    return codeLength;
+  }
+
+  public void setCodeLength(int codeLength) {
+    this.codeLength = codeLength;
+  }
+
+  @NonNull
+  public String getPool() {
+    return pool;
+  }
+
+  public void setPool(@NonNull String pool) {
+    this.pool = pool;
+  }
+
+  public int getGameCount() {
+    return gameCount;
+  }
+
+  public void setGameCount(int gameCount) {
+    this.gameCount = gameCount;
+  }
+
+  @NonNull
+  public Criterion getCriterion() {
+    return criterion;
+  }
+
+  public void setCriterion(@NonNull Criterion criterion) {
+    this.criterion = criterion;
+  }
+
+  @NonNull
+  public Date getDeadline() {
+    return deadline;
+  }
+
+  public void setDeadline(@NonNull Date deadline) {
+    this.deadline = deadline;
+  }
+
+  @NonNull
+  public User getOriginator() {
+    return originator;
+  }
+
+  public void setOriginator(@NonNull User originator) {
+    this.originator = originator;
+  }
+
+  public User getWinner() {
+    return winner;
+  }
+
+  public void setWinner(User winner) {
+    this.winner = winner;
+  }
+
+  @NonNull
+  public List<User> getPlayers() {
+    return players;
+  }
 
   public enum Criterion {
     GUESSES_TIME, TIME_GUESSES
--- src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/User.java
@@ -1,12 +1,19 @@
 package edu.cnm.deepdive.codebreaker.model.entity;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Index;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -53,6 +60,23 @@ public class User {
   private String displayName;
 
   @NonNull
+  @OneToMany(fetch = FetchType.LAZY, mappedBy = "originator", cascade = CascadeType.ALL,
+      orphanRemoval = true)
+  @OrderBy("started DESC")
+  private final List<Match> matchesOriginated = new LinkedList<>();
+
+  @NonNull
+  @OneToMany(mappedBy = "winner", fetch = FetchType.LAZY,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @OrderBy("deadline DESC")
+  private final List<Match> matchesWon = new LinkedList<>();
+
+  @NonNull
+  @ManyToMany(mappedBy = "players", fetch = FetchType.LAZY)
+  @OrderBy("deadline DESC")
+  private final List<Match> matches = new LinkedList<>();
+
+  @NonNull
   public UUID getId() {
     return id;
   }
@@ -87,6 +111,21 @@ public class User {
 
   public void setDisplayName(@NonNull String displayName) {
     this.displayName = displayName;
+  }
+
+  @NonNull
+  public List<Match> getMatchesOriginated() {
+    return matchesOriginated;
+  }
+
+  @NonNull
+  public List<Match> getMatchesWon() {
+    return matchesWon;
+  }
+
+  @NonNull
+  public List<Match> getMatches() {
+    return matches;
   }
 
 }
