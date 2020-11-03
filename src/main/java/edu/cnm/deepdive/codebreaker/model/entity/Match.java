1f18b2033e9db41ed3269857fdc305c8407ce42f
Nick Bennett <nick@nickbenn.com>
2020-11-03 11:51:14 -0700

    Beginning resource server functionality.

--- src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Match.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Match.java
@@ -26,6 +26,7 @@ import org.springframework.lang.NonNull;
 @SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 @Table(
+    name = "tournament",
     indexes = {
         @Index(columnList = "codeLength"),
         @Index(columnList = "gameCount"),
